6ae23de118d84676595e3ef8242370ba2d23bacc
Renan Campos <renanc433@gmail.com>
2021-02-19 11:09:36 -0300

    splash screen ios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import 'react-native-gesture-handler';
-
-import React from 'react';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen'
 import {Routes} from './Routes';
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
   return (
     <Routes/>
   );
